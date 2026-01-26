55eef2e1b4940f7225fdf1e12a47485b243be74b
Daniel Guimarães <danguimaraes86@gmail.com>
2026-01-26 14:57:34 -0300

    web: add: dashboard inicial e floating button

---
+++ fiap-hackathon-web/src/app/components/floating-button/floating-button.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { MatFabButton } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIcon } from "@angular/material/icon";
+import { TaskForm } from '../task-form/task-form';
+
+@Component({
+  selector: 'app-floating-button',
+  imports: [MatIcon, MatFabButton],
+  templateUrl: './floating-button.html',
+  styleUrl: './floating-button.scss',
+})
+export class FloatingButton {
+  private _dialogRef = inject(MatDialog)
+
+  openTaskForm() {
+    this._dialogRef.open(TaskForm, {
+      minWidth: '50%'
+    })
+  }
+
+}
--- fiap-hackathon-web/src/app/pages/dashboard-view/dashboard-view.ts
+++ fiap-hackathon-web/src/app/pages/dashboard-view/dashboard-view.ts
@@ -1,15 +1,26 @@
-import { JsonPipe } from '@angular/common';
-import { Component, effect, inject, OnDestroy } from '@angular/core';
+import { Component, computed, effect, inject, OnDestroy } from '@angular/core';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { FloatingButton } from "../../components/floating-button/floating-button";
 import { AuthenticationService } from '../../services/authentication.service';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-dashboard-view',
-  imports: [JsonPipe],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardActions, FloatingButton],
   templateUrl: './dashboard-view.html',
   styleUrl: './dashboard-view.scss',
 })
 export class DashboardView implements OnDestroy {
-  protected _authService = inject(AuthenticationService)
+  private _authService = inject(AuthenticationService)
+  private _taskService = inject(TaskService)
+
+  protected userDisplayName = computed(() => {
+    return this._authService.userSignal()?.displayName || 'Usuário'
+  })
+
+  protected welcomeMessage = computed(() => {
+    return `Hoje você tem ${this._taskService.tasks().length} tarefas importantes`
+  })
 
   private _effectRef = effect(() => {
     console.log(this._authService.isLoading())
