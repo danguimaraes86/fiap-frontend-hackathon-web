5e00de72d7c04dd482fa068278e431c6fec2ea97
Daniel Guimarães <danguimaraes86@gmail.com>
2026-03-02 15:50:07 -0300

    chore: general fixes

--- src/app/components/task-full-details/task-full-details.ts
+++ src/app/components/task-full-details/task-full-details.ts
@@ -1,22 +1,43 @@
 import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardContent } from "@angular/material/card";
 import { MatChip } from "@angular/material/chips";
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
 import { MatDivider } from "@angular/material/divider";
 import { MatIcon } from "@angular/material/icon";
 import { getTaskStatusInfo, Task } from '../../models/task.models';
 import { getDateLocaleString } from '../../utils/date-time.utils';
+import { TaskForm } from '../forms/task-form/task-form';
 
 @Component({
   selector: 'app-task-full-details',
-  imports: [MatDivider, MatIcon, MatCard, MatCardContent, MatChip],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatDivider,
+    MatIcon,
+    MatChip,
+    MatButton,
+  ],
   templateUrl: './task-full-details.html',
   styleUrl: './task-full-details.scss',
 })
 export class TaskFullDetails {
+  private _dialogRef = inject(MatDialog)
+
   protected payload = inject<{ task: Task }>(MAT_DIALOG_DATA);
   protected task = this.payload.task
 
   protected getDateLocaleString = getDateLocaleString
   protected getStatusInfo = getTaskStatusInfo
+
+  protected handleUpdateTask(task: Task) {
+    this._dialogRef.open(TaskForm, {
+      minWidth: '320px',
+      width: '60%',
+      data: { task }
+    })
+  }
 }
