c18bee03bb4374ca6a58841ceb277e2678b4d794
Daniel Guimarães <danguimaraes86@gmail.com>
2026-03-02 15:15:47 -0300

    refact: update style to mobile app

--- src/app/components/task-full-details/task-full-details.ts
+++ src/app/components/task-full-details/task-full-details.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
+import { MatCard, MatCardContent } from "@angular/material/card";
+import { MatChip } from "@angular/material/chips";
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDivider } from "@angular/material/divider";
+import { MatIcon } from "@angular/material/icon";
 import { getTaskStatusInfo, Task } from '../../models/task.models';
 import { getDateLocaleString } from '../../utils/date-time.utils';
 
 @Component({
   selector: 'app-task-full-details',
-  imports: [],
+  imports: [MatDivider, MatIcon, MatCard, MatCardContent, MatChip],
   templateUrl: './task-full-details.html',
   styleUrl: './task-full-details.scss',
 })
